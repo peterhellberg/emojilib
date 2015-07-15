f9521618c53b1dc309604361f6107962ab337035
Peter Hellberg <peter@c7.se>
2015-07-15 04:30:11 +0200

    A few more tests

--- emojilib_test.go
+++ emojilib_test.go
@@ -2,7 +2,56 @@ package emojilib
 
 import "testing"
 
-func TestReplacer(t *testing.T) {
+func TestFind(t *testing.T) {
+	for _, tt := range []struct {
+		in   string
+		want string
+		err  error
+	}{
+		{"unknown", "", ErrUnknownEmoji},
+		{"green_heart", "💚", nil},
+		{"bee", "🐝", nil},
+		{"scream", "😱", nil},
+		{"rocket", "🚀", nil},
+	} {
+		got, err := Find(tt.in)
+		if err != tt.err {
+			t.Errorf("unexpected error")
+		}
+
+		if got.Char != tt.want {
+			t.Errorf("Find(%q) = %q, nil, want %q, nil", tt.in, got.Char, tt.want)
+		}
+	}
+}
+
+func TestAll(t *testing.T) {
+	all := All()
+
+	if len(all) == 0 {
+		t.Fatalf("no emojis returned")
+	}
+
+	if len(all) != len(emojis) {
+		t.Fatalf("unexpected number of emojis returned")
+	}
+}
+
+func TestReplace(t *testing.T) {
+	for _, tt := range []struct {
+		in   string
+		want string
+	}{
+		{"foo :green_heart:  bar", "foo 💚  bar"},
+		{":sunny:  and :cloud: ", "☀️  and ☁️ "},
+	} {
+		if got := Replace(tt.in); got != tt.want {
+			t.Errorf("Replace(%q) = %q, want %q", tt.in, got, tt.want)
+		}
+	}
+}
+
+func TestReplaceWithPadding(t *testing.T) {
 	for _, tt := range []struct {
 		in   string
 		want string
